411225db2ba13c02e6394d22e1b3dc493f642ee9
Leo Smerling <61629371+leosmerling-hopeit@users.noreply.github.com>
2024-04-08 13:02:52 -0300

    pylint

--- plugins/build/ci-static-plugins.sh
+++ plugins/build/ci-static-plugins.sh
@@ -17,7 +17,7 @@ export MYPYPATH=plugins/aws/s3/src && python3 -m mypy --namespace-packages plugi
 code+=$?
 python3 -m flake8 plugins/aws/s3/src/hopeit/aws/s3 plugins/aws/s3/test/unit
 code+=$?
-python3 -m pylint plugins/aws/s3/src/hopeit/aws/s3 plugins/aws/s3/test/unit
+PYTHONPATH=plugins/aws/s3/src/ python3 -m pylint --recursive=y plugins/aws/s3/src/ plugins/aws/s3/test/
 code+=$?
 fi
 
